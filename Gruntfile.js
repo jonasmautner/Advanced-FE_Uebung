0a215439e6f46652c84997045670cc16d0a294e9
jonas <s1910456019@students.fh-hagenberg.at>
2022-04-26 17:48:10 +0200

    Optimize structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
             }
         },
         sass: {
-            dist: {
+            css: {
                 src: 'assets/css/main.scss',
                 dest: 'assets/css/main.css',
                 options: {
@@ -25,21 +25,23 @@ module.exports = function(grunt) {
         concat: {
             css: {
                 src: 'node_modules/bootstrap/dist/css/bootstrap*.min.css',
-                dest: 'assets/css/bootstrap.main.min.css'
+                dest: 'assets/bootstrap/bootstrap.main.min.css'
             },
             script: {
-                src: 'node_modules/bootstrap/dist/js/bootstrap*.min.js',
-                dest: 'assets/js/bootstrap.main.min.js'
+                files: {
+                    'assets/bootstrap/bootstrap.main.min.js': 'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+                    'assets/js/main.js': 'assets/js/_*.js'
+                }
             }
         },
         cssmin: {
-            dist: {
+            css: {
                 src: 'assets/css/main.css',
                 dest: 'assets/css/main.min.css'
             }
         },
         uglify: {
-            dist: {
+            script: {
                 src: 'assets/js/main.js',
                 dest: 'assets/js/main.min.js'
             }
@@ -51,6 +53,5 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-
     grunt.registerTask('default', ['sass', 'concat', 'cssmin', 'uglify']);
 };
--- assets/js/_navigation.js
+++ assets/js/_navigation.js
@@ -1,2 +1,1 @@
-console.log("_footer.js file");
 console.log("_navigation.js file");
